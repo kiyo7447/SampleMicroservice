d7fc5459ed404cd19644acc6fbd78a8a0093933d
Kiyotaka Abe <kiyo7447@gmaill.com>
2023-03-06 23:44:55 +0900

    プロジェクト ファイルを追加します。

---
+++ BatchInsertMySQLConsoleApp/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+
+namespace BatchInsertExample
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// データベース接続情報の設定
+			string connectionString = "server=localhost;user=root;password=password;database=mydb";
+
+			// データの読み取り
+			List<string[]> data = ReadDataFromCsv("data.csv");
+
+			// バッチ挿入の設定
+			int batchSize = 1000;
+
+			// バッチ挿入の実行
+			using (MySqlConnection connection = new MySqlConnection(connectionString))
+			{
+				connection.Open();
+				using (MySqlCommand command = new MySqlCommand())
+				{
+					command.Connection = connection;
+					command.CommandType = CommandType.Text;
+					command.CommandText = "INSERT INTO products (name, price) VALUES (@name, @price)";
+
+					for (int i = 0; i < data.Count; i += batchSize)
+					{
+						int count = Math.Min(batchSize, data.Count - i);
+						for (int j = 0; j < count; j++)
+						{
+							command.Parameters.AddWithValue("@name" + j, data[i + j][0]);
+							command.Parameters.AddWithValue("@price" + j, int.Parse(data[i + j][1]));
+						}
+						command.ExecuteNonQuery();
+						command.Parameters.Clear();
+					}
+				}
+				connection.Close();
+			}
+		}
+
+		static List<string[]> ReadDataFromCsv(string fileName)
+		{
+			List<string[]> data = new List<string[]>();
+			using (StreamReader reader = new StreamReader(fileName))
+			{
+				while (!reader.EndOfStream)
+				{
+					string line = reader.ReadLine();
+					string[] values = line.Split(',');
+					data.Add(values);
+				}
+			}
+			return data;
+		}
+	}
+}
---
+++ S32MySQLConsoleApp/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+using MySql.Data.MySqlClient;
+
+namespace ProductMasterImporter
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			// Amazon S3 の認証情報を設定します
+			var s3Config = new AmazonS3Config
+			{
+				RegionEndpoint = RegionEndpoint.USEast1,
+				// 以下は、必要に応じて設定してください
+				//  Credentials = new Amazon.Runtime.BasicAWSCredentials("accessKeyId", "secretAccessKey")
+			};
+			var s3Client = new AmazonS3Client(s3Config);
+
+			// Amazon S3 バケットから CSV ファイルをダウンロードします
+			var getObjectRequest = new GetObjectRequest
+			{
+				BucketName = "s3-bucket-name",
+				Key = "product_master.csv"
+			};
+			using var getObjectResponse = await s3Client.GetObjectAsync(getObjectRequest);
+			using var streamReader = new StreamReader(getObjectResponse.ResponseStream);
+			var csvData = await streamReader.ReadToEndAsync();
+
+			// AWS Aurora データベースへの接続情報を設定します
+			var connectionStringBuilder = new MySqlConnectionStringBuilder
+			{
+				Server = "database-host-name",
+				Database = "database-name",
+				UserID = "database-user-id",
+				Password = "database-password"
+			};
+			using var connection = new MySqlConnection(connectionStringBuilder.ConnectionString);
+
+			// 商品マスタテーブルに CSV ファイルのデータを挿入します
+			var insertCommand = new MySqlCommand
+			{
+				Connection = connection,
+				CommandText = "INSERT INTO product_master (product_code, product_name, price) VALUES (@product_code, @product_name, @price);"
+			};
+			insertCommand.Parameters.Add("@product_code", MySqlDbType.VarChar);
+			insertCommand.Parameters.Add("@product_name", MySqlDbType.VarChar);
+			insertCommand.Parameters.Add("@price", MySqlDbType.Int32);
+			await connection.OpenAsync();
+			foreach (var line in csvData.Split('\n'))
+			{
+				var values = line.Split(',');
+				if (values.Length == 3)
+				{
+					insertCommand.Parameters["@product_code"].Value = values[0];
+					insertCommand.Parameters["@product_name"].Value = values[1];
+					insertCommand.Parameters["@price"].Value = int.Parse(values[2]);
+					await insertCommand.ExecuteNonQueryAsync();
+				}
+			}
+			await connection.CloseAsync();
+		}
+	}
+}
